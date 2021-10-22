fce85e117691bdf9e634a74231bab91be9e27717
Headgrass <headgrass@ngs.ru>
2021-10-22 08:14:47 +0700

    HW for Lesson 2

--- app/src/main/java/ru/geekbrains/android1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1/MainActivity.java
@@ -12,17 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn_settings).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.settings);
-            }
-        });
-        findViewById(R.id.btn_anketa).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.test_ui);
-            }
-        });
+        findViewById(R.id.btn_settings).setOnClickListener(v -> setContentView(R.layout.settings));
+        findViewById(R.id.btn_anketa).setOnClickListener(v -> setContentView(R.layout.test_ui));
+        findViewById(R.id.btn_calc).setOnClickListener(v -> setContentView(R.layout.calc));
+
     }
 }
